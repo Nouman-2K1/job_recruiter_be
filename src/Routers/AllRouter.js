4f12e7603e903b03487705d81c4555f5ff2416b4
Muhammad Noman <m.nouman.nomi@gmail.com>
2024-01-13 00:04:02 +0500

    implemented Company controler, service, and router to get all Companies

--- src/Routers/AllRouter.js
+++ src/Routers/AllRouter.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import AuthRouter from "./AuthRouter/AuthRouter.js";
 import AdminAuthRouter from "./AdminAuthRouter/AdminAuthRouter.js";
 import JobRouter from "./JobRouter/JobRouter.js";
+import CompanyRouter from "./CompanyRouter/CompanyRouter.js";
 const AllRouter = Router();
 
 AllRouter.use("/auth/user", AuthRouter);
 AllRouter.use("/auth/admin", AdminAuthRouter);
 AllRouter.use("/", JobRouter);
+AllRouter.use("/", CompanyRouter);
 
 export default AllRouter;
